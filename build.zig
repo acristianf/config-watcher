f1a5ad64248bb8e8526c60376a5b9f6d4c8b63fa
Cristian Ferretti <acristianf@hotmail.com>
2024-07-23 10:33:53 -0300

    moved everything to src/

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) !void {
     const exe = b.addExecutable(.{
         .name = "config-watcher",
-        .root_source_file = b.path("main.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = b.graph.host,
     });
     exe.linkLibC();
